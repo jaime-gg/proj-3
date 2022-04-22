4a83e2f26ed8661d7b2c52aa8d075cb31ee13790
jester <jaimebuddy98@gmail.com>
2022-04-22 13:02:32 -0500

    established mutation typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -42,7 +42,11 @@ const typeDefs = gql`
     }
 
     type Mutation {
-
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        addOrder(products: [ID]!): Order
+        updateUser(firstName: String, lastName: String, email: String, password: String): User
+        updateProduct(_id: ID!, quantity: Int!): Product
+        login(email: String!, password: String!): Auth
     }
 `;
 
