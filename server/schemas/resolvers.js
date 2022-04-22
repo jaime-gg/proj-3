98e3a04ea96f3d8a4d041b23d570c45a72b7f597
jester <jaimebuddy98@gmail.com>
2022-04-22 13:22:49 -0500

    product, user, and order | query resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,42 @@
-// const { AuthenticationError } = require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express');
 const { Books } = require('../models');
-// const { signToken } = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 // const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 
 const resolvers = {
     Query: {
+        product: async (parent, { _id }) => {
+            return await Product.findById(_id).populate('category');
+        },
 
+        user: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id).populate({
+                    path: 'orders.products',
+                    populate: 'category'
+                });
+
+                user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+
+                return user;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+
+        order: async (parent, { _id }, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id).populate({
+                    path: 'orders.products',
+                    // populate: 'category'
+                });
+
+                return user.orders.id(_id);
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
     },
     Mutation: {
 
