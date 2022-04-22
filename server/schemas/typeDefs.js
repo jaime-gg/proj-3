840a0d1ff52421bb0d6b337d3dd7f9489979301c
jester <jaimebuddy98@gmail.com>
2022-04-22 12:58:33 -0500

    defined needed types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,38 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Books {
+        _id: ID
+        name: String
+        description: String
+        image: String
+        quantity: Int
+        price: Float
+    }
+
+    type User {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        orders: [Order]
+    }
+
+    type Order {
+        _id: ID
+        purchaseDate: String
+        products: [Product]
+    }
+
+    type Checkout {
+        session: ID
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
 
     type Query {
     
