0c600f156b155b71aced093cd001df48306e9aa3
jester <jaimebuddy98@gmail.com>
2022-04-22 13:16:32 -0500

    order model schema fields established

--- server/models/Order.js
+++ server/models/Order.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const orderSchema = new Schema({
-
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ], 
+    purchaseDate: {
+        type: Date,
+        default: Date.now
+    },
 });
 
 const Order = mongoose.model('Order', orderSchema);
