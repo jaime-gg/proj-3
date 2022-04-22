9f0aa94d4c461614bd9b7a880a52851b1fbda764
jester <jaimebuddy98@gmail.com>
2022-04-22 13:13:06 -0500

    basic user password hashing middleware

--- server/models/User.js
+++ server/models/User.js
@@ -7,6 +7,21 @@ const userSchema = new Schema({
 });
 
 
+// SET UP PRE-SAVE MIDDLEWARE TO CREATE PASSWORD
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// COMPARE THE INCOMING PASSWORD WITH THE HASHED PASSWORD
+userSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
