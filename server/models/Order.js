06cf65b93f0f7b1de28717fb6e9d1557db645cfa
jester <jaimebuddy98@gmail.com>
2022-04-22 13:15:41 -0500

    basic order model layout and intigration

---
+++ server/models/Order.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
