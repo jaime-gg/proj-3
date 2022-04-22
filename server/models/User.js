2e69913a19a27c67c99d30a2c07df2a002e470d6
jester <jaimebuddy98@gmail.com>
2022-04-22 13:14:08 -0500

    user model schema fields established

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,26 @@ const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
 });
 
 
