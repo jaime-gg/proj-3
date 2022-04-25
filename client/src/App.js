815f99c3510fa10c4f7843f365a8ec2c0a81901d
jester <jaimebuddy98@gmail.com>
2022-04-25 14:14:19 -0500

    setup barebones app layout

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { StoreProvider } from "./utils/GlobalState";
+
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+// import Home from './pages/Home';
+// import Nav from './components/Nav';
+
+
+const httpLink = createHttpLink({
+    uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <StoreProvider>
+                        {/* <Nav /> */}
+                        <Switch>
+                            {/* <Route exact path="/" component={Home} />
+                            <Route component={NoMatch} /> */}
+                        </Switch>
+                    </StoreProvider>
+                </div>
+            </Router>
+        </ApolloProvider>
+    );
+}
+
+export default App;
