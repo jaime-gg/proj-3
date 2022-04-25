079dba8fd7ccf47035cc98e8ec711f45d4fd84d5
jester <jaimebuddy98@gmail.com>
2022-04-25 14:18:33 -0500

    establish global context | StoreProvider and Context

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext } from "react";
+import { useBookReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+
+const StoreProvider = ({value = [], ...props}) => {
+    const [state, dispatch] = useBookReducer({
+        books: [],
+        cart: [],
+        cartOpen: false,
+        filters: [],
+        currentFilter: ''
+    })
+    console.log(state) // use this in order to confirm it works
+    return <Provider value={[state, dispatch]} {...props} />
+}
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
