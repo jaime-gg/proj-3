c352027f232232a50ef3fa93f0938b23e23ad657
jester <jaimebuddy98@gmail.com>
2022-04-26 13:27:57 -0500

    basic seeds for filters and vendor

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,21 +1,48 @@
 const db = require('./connection');
 // IMPORT AND CREATE SEEDS FOR RESPECTIVE MODALS 
-const { Books } = require('../models');
+const { User, Book, Filter } = require('../models');
 
 db.once('open', async () => {
-    await Books.deleteMany();
-    const Books = await Books.insertMany([
+    await Filter.deleteMany();
+    const filters = await Filter.insertMany([
+        { name: 'Art' },
+        { name: 'Collections' },
+        { name: 'Crime' },
+        { name: 'Fiction' },
+        { name: 'History' },
+        { name: 'Science' },
+        { name: 'Textbook' },
+        { name: 'Non-Fiction' }
+    ]);
+    console.log('filters have been seeded');
+
+    // ================================================================================================
+
+    await Book.deleteMany();
+    const Books = await Book.insertMany([
 
         {
-            name: '',
-            description:'',
-            image: '',
-            // category: categories[0]._id,
-            price: ,
-            quantity: 
+            name: 'Death of a Stray Cat',
+            description: "When a woman's body is discovered at her lover's villa, the police discover a trail of lovers - all of whom have a reason to wish her dead.",
+            image: 'stray-1962.jpg',
+            filters: filters[3, 4]._id,
+            price: 75.99,
+            quantity: 5
         },
 
     ])
+    console.log('books have been seeded');
+
+    // ================================================================================================
+    
+    await User.deleteMany();
+    await User.create({
+        firstName: 'John',
+        lastName: 'Smith',
+        email: 'vendor@mail.com',
+        password: 'password123',
+    });
+    console.log('users seeded');
 
     process.exit();
 });
