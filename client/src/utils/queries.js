9a58fc5ab2ce2f3d9034a03d866a3ef32842b833
jester <jaimebuddy98@gmail.com>
2022-04-26 12:52:37 -0500

    query books and filters

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,41 @@
 import { gql } from '@apollo/client';
 
-// export const QUERY_BOOKS = gql`
-    // query getProducts($category: ID) {
-    
-    // }
-// `;
+export const QUERY_ALL_BOOKS = gql`
+    {
+        books {
+            _id
+            name
+            description
+            price
+            quantity
+            filter {
+                name
+            }
+        }
+    }
+`;
+
+export const QUERY_BOOKS = gql`
+    query getBooks($category: ID) {
+        books(filter: $filter) {
+            _id
+            name
+            description
+            price
+            quantity
+            image
+            filter {
+                _id
+            }
+        }
+    }
+`;
+
+export const QUERY_FILTERS = gql`
+    {
+        filters {
+            _id
+            name
+        }
+    }
+`;
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -32,6 +32,58 @@ export const reducer = (state, action) => {
                 ...state,
                 currentFilter: action.currentFilter
             }
+
+
+
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.book]
+            }
+
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.books]
+            }
+
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(book => {
+                return book._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            };
+
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(book => {
+                    if (action._id === book._id) {
+                        book.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return book;
+                })
+            };
+
+        case CLEAR_CART: 
+            return {
+                ...state,
+                cartOpen: false, 
+                cart: []
+            }
+
+        case TOGGLE_CART: 
+            return {
+                ...state, 
+                cartOpen: !state.cartOpen
+            }
+
         // SET THIS AS DEFAULT
         default:
             return state;
