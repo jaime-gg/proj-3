837a09ba1502a1c947031b7f119d100e42393cab
jester <jaimebuddy98@gmail.com>
2022-04-26 12:54:29 -0500

    query users and checkout

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,7 +16,7 @@ export const QUERY_ALL_BOOKS = gql`
 `;
 
 export const QUERY_BOOKS = gql`
-    query getBooks($category: ID) {
+    query getBooks($filter: ID) {
         books(filter: $filter) {
             _id
             name
@@ -36,6 +36,35 @@ export const QUERY_FILTERS = gql`
         filters {
             _id
             name
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    {
+        user {
+            firstName
+            lastName
+            orders {
+                _id
+                purchaseDate
+                books {
+                _id
+                name
+                description
+                price
+                quantity
+                image
+                }
+            }
+        }
+    }
+`;
+
+export const QUERY_CHECKOUT = gql`
+    query getCheckout($books: [ID]!) {
+        checkout(books: $books) {
+            session
         }
     }
 `;
