f930aa80444ee202d380b908a048996c09927ad7
dit <ditazanelli@gmail.com>
2022-04-29 14:29:23 -0500

    book cover path was incorrect

--- client/build/precache-manifest.a45390d330517049dac9c7fe9c34ab22.js
+++ client/build/precache-manifest.a45390d330517049dac9c7fe9c34ab22.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cb168b263380b83d6c4a3217c51f0c95",
+    "revision": "b03752f7f0ca96f1f67677a131b24bfa",
     "url": "/index.html"
   },
   {
-    "revision": "ebd664926724fbf14665",
+    "revision": "111e05505107cfcadd15",
     "url": "/static/css/2.68a81a89.chunk.css"
   },
   {
-    "revision": "663bd785f86b5b98948a",
-    "url": "/static/css/main.d5eac77c.chunk.css"
+    "revision": "f97bfab89da453e10048",
+    "url": "/static/css/main.8571186d.chunk.css"
   },
   {
-    "revision": "ebd664926724fbf14665",
-    "url": "/static/js/2.c47eaadb.chunk.js"
+    "revision": "111e05505107cfcadd15",
+    "url": "/static/js/2.0c7f4013.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/static/js/2.c47eaadb.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.0c7f4013.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "663bd785f86b5b98948a",
-    "url": "/static/js/main.7e222db8.chunk.js"
+    "revision": "f97bfab89da453e10048",
+    "url": "/static/js/main.2a9c6051.chunk.js"
   },
   {
     "revision": "02751e160f988a50d44f",
