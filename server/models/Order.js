21dc33b8c9bae1d7b11f909a89fe129c0ea83805
jester <jaimebuddy98@gmail.com>
2022-05-03 19:15:58 -0500

    attempting to add functional checkout query + resolver

--- server/models/Order.js
+++ server/models/Order.js
@@ -3,16 +3,17 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const orderSchema = new Schema({
+    purchaseDate: {
+        type: Date,
+        default: Date.now
+    },
+    
     books: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Book'
         }
     ], 
-    purchaseDate: {
-        type: Date,
-        default: Date.now
-    },
 });
 
 const Order = mongoose.model('Order', orderSchema);
