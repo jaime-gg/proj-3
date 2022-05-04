9b7a4488205b41d97dcc817c06ab097cf136e60e
jester <jaimebuddy98@gmail.com>
2022-05-04 15:49:27 -0500

    dotenv functionality

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/textosAntiguos', {
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_NAME, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
